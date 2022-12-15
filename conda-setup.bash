810e30c7f866303d1526b5c7bceca9afeb1ec498
Paul Stodghill <paul.stodghill@usda.gov>
2022-12-15 12:27:18 -0500

    Rework to standard format.

--- conda-setup.bash
+++ conda-setup.bash
@@ -2,21 +2,30 @@
 
 set -e
 
+NAME=pipeline-genomes
+
 CONDA_PREFIX=$(dirname $(dirname $(type -p conda)))
 . "${CONDA_PREFIX}/etc/profile.d/conda.sh"
 
 PACKAGES=
+#PACKAGES+=" pip"
 
 PACKAGES+=" ncbi-datasets-cli"
 PACKAGES+=" prokka"
 PACKAGES+=" busco"
 
+if [ "$(type -p mamba)" ] ; then
+    _conda="mamba --no-banner"
+else
+    _conda=conda
+fi
+
 set -x
 
-conda env remove -y --name pipeline-genomes
-conda create -y --name pipeline-genomes
-conda activate pipeline-genomes
+conda env remove -y --name ${NAME}
+conda create -y --name ${NAME}
+conda activate ${NAME}
 
-conda install -y ${PACKAGES}
+$_conda install -y ${PACKAGES}
 
-pip install pyparanoid
+#pip install FIXME
