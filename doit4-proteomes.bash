4aafc8722b6f660f558d7397c078219e4826f25d
Paul Stodghill <paul.stodghill@usda.gov>
2022-12-23 12:20:44 -0500

    doit4-proteomes.bash: rewrite
    - STRAIN -> ACCESSION

--- doit4-proteomes.bash
+++ doit4-proteomes.bash
@@ -12,13 +12,13 @@ rm -rf ${PROTEOMES}
 mkdir -p ${PROTEOMES}
 
 for FNA in ${GENOMES}/*.fna ; do
-    STRAIN=$(basename $FNA .fna)
-    if [ -e ${PROKKA}/${STRAIN}.faa ] ; then
-	cp ${PROKKA}/${STRAIN}.faa ${PROTEOMES}/${STRAIN}.faa
-    elif [ -e ${GENOMES}/${STRAIN}.faa ] ; then
-	cp ${GENOMES}/${STRAIN}.faa ${PROTEOMES}/${STRAIN}.faa
+    ACCESSION=$(basename $FNA .fna)
+    if [ -e ${PROKKA}/${ACCESSION}.faa ] ; then
+	cp ${PROKKA}/${ACCESSION}.faa ${PROTEOMES}/${ACCESSION}.faa
+    elif [ -e ${GENOMES}/${ACCESSION}.faa ] ; then
+	cp ${GENOMES}/${ACCESSION}.faa ${PROTEOMES}/${ACCESSION}.faa
     else
-	echo 1>&2 "Cannot find: ${STRAIN}.faa"
+	echo 1>&2 "Cannot find: ${ACCESSION}.faa"
 	exit 1
     fi
 done
