b1e80e5a48f0cc17fe1ba1498d9c9cffb61b6b63
Paul Stodghill <paul.stodghill@usda.gov>
2022-12-27 15:48:46 -0500

    doit6-mashtree.bash: edits
    - Write tree output to mashtree.dnd
    - Compute clusters using mashtree_cluster.pl.

--- doit6-mashtree.bash
+++ doit6-mashtree.bash
@@ -9,13 +9,22 @@
 echo 1>&2 '# Running mashtree...'
 
 rm -rf ${MASHTREE} ${DATA}/mashtree.tsv
-mkdir -p ${MASHTREE}
+mkdir -p ${MASHTREE}/tmp
 
 mashtree \
     --numcpus ${THREADS} \
     --outmatrix ${MASHTREE}/mashtree.tsv \
-    --outtree ${MASHTREE}/mashtree.tsv \
+    --outtree ${MASHTREE}/mashtree.dnd \
+    --mindepth 0 \
+    --tempdir ${MASHTREE}/tmp \
     ${GENOMES}/*.fna \
+
+mashtree_cluster.pl \
+    --threshold 0.05 \
+    ${MASHTREE}/tmp/distances.sqlite \
+    | sort > ${MASHTREE}/clusters.txt
+
+
 
 cp --archive ${MASHTREE}/mashtree.tsv ${DATA}
 
