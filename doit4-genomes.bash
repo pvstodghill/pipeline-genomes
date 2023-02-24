fff1950bfc5fc9295370ff1b19bc73f84e3c05be
Paul Stodghill <paul.stodghill@usda.gov>
2023-02-24 15:48:28 -0500

    genomes is now stage 4. use accession for filenames instead of "name".

--- doit4-genomes.bash
+++ doit4-genomes.bash
@@ -12,17 +12,17 @@ rm -rf ${GENOMES}
 mkdir -p ${GENOMES}
 
 cat ${RAW}/_metadata_.tsv | (
-    while IFS=$'\t' read NAME IGNORED ; do
-	if [ "${NAME}" = "Name" ] ; then
+    while IFS=$'\t' read ACCESSION IGNORED ; do
+	if [ "${ACCESSION}" = "Accession" ] ; then
 	    continue
 	fi
 	for EXT in fna faa gff ; do
-	    if [ -e ${PROKKA}/${NAME}_prokka/output.${EXT} ] ; then
-		cp ${PROKKA}/${NAME}_prokka/output.${EXT} ${GENOMES}/${NAME}.${EXT}
-	    elif [ -e ${RAW}/${NAME}.${EXT} ] ; then
-		cp ${RAW}/${NAME}.${EXT} ${GENOMES}/${NAME}.${EXT}
+	    if [ -e ${PROKKA}/${ACCESSION}_prokka/output.${EXT} ] ; then
+		cp ${PROKKA}/${ACCESSION}_prokka/output.${EXT} ${GENOMES}/${ACCESSION}.${EXT}
+	    elif [ -e ${RAW}/${ACCESSION}.${EXT} ] ; then
+		cp ${RAW}/${ACCESSION}.${EXT} ${GENOMES}/${ACCESSION}.${EXT}
 	    else
-		echo 1>&2 "Cannot find: ${NAME}.${EXT}"
+		echo 1>&2 "Cannot find: ${ACCESSION}.${EXT}"
 		exit 1
 	    fi
 	done
